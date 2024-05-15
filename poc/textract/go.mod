c34005b5661aa0665d4c686775f1952f26f08365
yamamotodin <yamamotodin@gmail.com>
2024-05-16 02:35:08 +0900

    結論

--- poc/textract/go.mod
+++ poc/textract/go.mod
@@ -3,8 +3,12 @@ module testract
 go 1.22.3
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.13
+	github.com/aws/aws-sdk-go-v2/service/textract v1.30.6
+)
+
+require (
 	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.27.13 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.13 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
@@ -15,6 +19,5 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.20.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.24.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.7 // indirect
-	github.com/aws/aws-sdk-go-v2/service/textract v1.30.6 // indirect
 	github.com/aws/smithy-go v1.20.2 // indirect
 )
--- poc/textract/main.go
+++ poc/textract/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"time"
-
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/textract"
+	"github.com/aws/aws-sdk-go-v2/service/textract/types"
+	"time"
 )
 
 func main() {
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("ap-northeast-1"),
+		config.WithRegion("ap-northeast-2"),
 	)
 	if err != nil {
 		fmt.Println("Configuration error", err)
@@ -19,14 +20,17 @@ func main() {
 	}
 	client := textract.NewFromConfig(cfg)
 
+	bucket := "test-textract-kujirabo-apne2"
+	key := "recept02.png"
+
 	startDocTextDetectionInput := &textract.StartDocumentTextDetectionInput{
-		DocumentLocation: &textract.DocumentLocation{
-			S3Object: &textract.S3Object{
+		DocumentLocation: &types.DocumentLocation{
+			S3Object: &types.S3Object{
 				Bucket: &bucket,
-				Name:   &documentName,
+				Name:   &key,
 			},
 		},
-		FeatureTypes: []textract.FeatureType{textract.FeatureTypeTables},
+		//FeatureTypes: []textract.FeatureType{textract.FeatureTypeTables},
 	}
 	startResponse, err := client.StartDocumentTextDetection(context.TODO(), startDocTextDetectionInput)
 	if err != nil {
@@ -37,28 +41,44 @@ func main() {
 	fmt.Println("Job ID:", jobId)
 
 	// ジョブの完了を待機する処理
-
+LOOP:
 	for {
-		describeResponse, err := client.DescribeDocumentTextDetection(context.TODO(), &textract.DescribeDocumentTextDetectionInput{
+		output, err := client.GetDocumentTextDetection(context.TODO(), &textract.GetDocumentTextDetectionInput{
 			JobId: jobId,
 		})
 		if err != nil {
-			fmt.Println("Error describing text detection job", err)
+			fmt.Println("Error getting text detection results", err)
 			return
 		}
-		if describeResponse.JobStatus == textract.JobStatusSucceeded {
-			break
+		fmt.Println(output.JobStatus)
+		switch output.JobStatus {
+		case types.JobStatusFailed:
+			fmt.Println("Error describing text detection job", err)
+			break LOOP
+		case types.JobStatusInProgress:
+			fmt.Println("continue..")
+			time.Sleep(5 * time.Second)
+			continue LOOP
+		case types.JobStatusSucceeded:
+		case types.JobStatusPartialSuccess:
 		}
-		// 5秒待機
-		time.Sleep(5 * time.Second)
+		fmt.Println("textract:")
+		b, err := json.Marshal(output)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(b))
+		break
+		//describeResponse, err := client.(context.TODO(), &types.DescribeDocumentTextDetectionInput{
+		//	JobId: jobId,
+		//})
+		//if err != nil {
+		//	fmt.Println("Error describing text detection job", err)
+		//	return
+		//}
+		//if describeResponse.JobStatus == textract.JobStatusSucceeded {
+		//	break
+		//}
 	}
-
-	getResponse, err := client.GetDocumentTextDetection(context.TODO(), &textract.GetDocumentTextDetectionInput{
-		JobId: jobId,
-	})
-	if err != nil {
-		fmt.Println("Error getting text detection results", err)
-		return
-	}
-	// 結果の処理
 }
